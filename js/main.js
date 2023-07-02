ab7e77b2e541fab8377e8e91f664f7c8742090b2
sasha <sasha1235qaz@ukr.net>
2023-07-01 19:14:58 -0700

    contactswrapwithjs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector('.mobile-full-menu');
 const burger = document.querySelector('.burger');
 const burger2 = document.querySelector('.burger2');
+const contactsHeadMenuBtn = document.querySelector('#contacts-headmenu');
+const headMenuContainer = document.querySelector('#headmenu-container');
 
 burger.addEventListener('click', () => {
   menu.style = 'display: flex';
@@ -8,10 +10,18 @@ burger.addEventListener('click', () => {
 burger2.addEventListener('click', () => {
   menu.style = 'display: none';
 });
+contactsHeadMenuBtn.addEventListener('click', () => {
+  contactsContainer.classList.add('show');
+})
+contactsHeadMenuBtn.addEventListener('click', () => {
+  menu.style = 'display:none';
+})
+
 
 const orderServiceBtn = document.querySelector('#order-service-btn');
 const orderServiceBtnClose = document.querySelector('#order-service-btn-close');
 const orderServiceContainer = document.querySelector('#order-service-container');
+const orderInContacts = document.querySelector('#orderservice-contacts');
 
 orderServiceBtn.addEventListener('click', () => {
   orderServiceContainer.classList.add('show');
@@ -19,3 +29,21 @@ orderServiceBtn.addEventListener('click', () => {
 orderServiceBtnClose.addEventListener('click', () => {
   orderServiceContainer.classList.remove('show');
 });
+orderInContacts.addEventListener('click', () => {
+  orderServiceContainer.classList.add('show');
+})
+
+const contactsBtn = document.querySelector('#contacts-btns');
+const contactsContainer = document.querySelector('#contacts-container');
+const contactsRemove = document.querySelector('#contacts-remove');
+const orderIncontactsremove = document.querySelector('#orderservice-contacts');
+
+contactsBtn.addEventListener('click', () => {
+  contactsContainer.classList.add('show');
+});
+contactsRemove.addEventListener('click', () => {
+  contactsContainer.classList.remove('show');
+})
+orderIncontactsremove.addEventListener('click', () => {
+  contactsContainer.classList.remove('show');
+})
